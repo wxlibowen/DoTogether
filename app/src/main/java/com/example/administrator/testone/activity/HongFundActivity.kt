7d55710ba7f7b0fd7bbcc63886d3b0e75dc3ae58
LBW <wxlibowen@hotmail.com>
2018-12-23 13:34:27 +0800

    组队系统修改

--- app/src/main/java/com/example/administrator/testone/activity/HongFundActivity.kt
+++ app/src/main/java/com/example/administrator/testone/activity/HongFundActivity.kt
@@ -4,11 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.support.v4.content.ContextCompat
-import android.view.View
-import android.view.Window
 import android.view.WindowManager
-import android.widget.Button
-
 import com.example.administrator.testone.R
 import com.example.administrator.testone.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_hong_fund.*
@@ -17,9 +13,37 @@ import org.jetbrains.anko.image
 import java.util.*
 
 class HongFundActivity : BaseActivity() {
-    private var i = 30
+    private var i = 0
     private val mList = ArrayList<Int>()
     private val handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hong_fund)
+                //修改状态栏颜色
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = ContextCompat.getColor(this@HongFundActivity,R.color.colorGray)
+        }
+
+
+
+        mList.add(R.drawable.xiaobo)
+        mList.add(R.drawable.bowen)
+        mList.add(R.drawable.taishanyading)
+        mList.add(R.drawable.tangyue)
+        mList.add(R.drawable.mingxing)
+        mList.add(R.drawable.chenbin)
+        mDaLuanBTN.setOnClickListener {
+            if (i>0) return@setOnClickListener
+            i = 30
+            diyizu.backgroundColor=ContextCompat.getColor(this@HongFundActivity,R.color.colorGray)
+            dierzu.backgroundColor=ContextCompat.getColor(this@HongFundActivity,R.color.colorGray)
+            disanzu.backgroundColor=ContextCompat.getColor(this@HongFundActivity,R.color.colorGray)
+            handler.post(runnable)
+        }
+    }
     private val runnable = object : Runnable {
         override fun run() {
             mList.shuffle()
@@ -41,38 +65,7 @@ class HongFundActivity : BaseActivity() {
                 i in 10..20 -> handler.postDelayed(this, 200)
                 i in 5..10 -> handler.postDelayed(this, 300)
                 i < 5->handler.postDelayed(this, 1000)
-                else -> handler.postDelayed(this, 300)
             }
-        }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hong_fund)
-                //修改状态栏颜色
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val window = this.window
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.statusBarColor = ContextCompat.getColor(this@HongFundActivity,R.color.colorGray)
-        }
-
-
-
-        mList.add(R.drawable.xiaobo)
-        mList.add(R.drawable.bowen)
-        mList.add(R.drawable.shangzhi)
-        mList.add(R.drawable.tangyue)
-        mList.add(R.drawable.mingxing)
-        mList.add(R.drawable.chenbin)
-        mDaLuanBTN.setOnClickListener {
-            i = 30
-            diyizu.backgroundColor=ContextCompat.getColor(this@HongFundActivity,R.color.colorGray)
-            dierzu.backgroundColor=ContextCompat.getColor(this@HongFundActivity,R.color.colorGray)
-            disanzu.backgroundColor=ContextCompat.getColor(this@HongFundActivity,R.color.colorGray)
-            handler.post(runnable)
-
-
         }
     }
 }
